d2b89935ed104d082a792dd1c1be58cff4be4011
Dexter84 <fenixzaec84@gmail.com>
2024-11-30 13:23:09 +0700

    lesson26 complteted!

--- script.js
+++ script.js
@@ -1,76 +1,37 @@
-const form = document.querySelector('.form')
 const getButton = document.querySelector('.get')
-const sendButton = document.querySelector('.send')
 
 let getText = document.querySelector('.get-result')
 let sendText = document.querySelector('.send-result')
 
-const getData = ({ url, data, method }) => {
-
-    return fetch(url, {
-        data: data,
-        method: method
-    })
-    .then(response => response.json())  
-    .then(() => {
-        sendData({
-            url: 'https://jsonplaceholder.typicode.com/posts',
-            data: data,
-            method: 'POST'
-        })
+const getData = () => {
+    fetch('db.json')
+    .then((response) => {
+        return response.json()})  
+    .then((data) => {
+        getText.textContent = 'Получено!'
+        sendData(data)
     })
     .catch(() => {
         getText.textContent = 'Не получено :('
     })
 }
-
-const sendData = ({ url, data, method }) => {
-
-    return fetch(url, {
-        data: data,
-        method: method,
+const sendData = (data) => {
+    const newData = JSON.stringify(data)
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: newData,
         headers: { 
             'Content-type': 'multipart/form-data',
-          },
-    }).then(response => response.json())  
+          }
+    })
     .then(() => {
         sendText.textContent = 'Отправлено!'
     })
     .catch(() => {
-        getText.textContent = 'Не отправлено :('
+        sendText.textContent = 'Не отправлено :('
     })
 }
+
 getButton.addEventListener('click', () => {
-
-    const data = new FormData(form)
-
-    getData({
-        url: 'db.json',
-        data: data,
-        method: 'GET'
-    })
-    .then(() => {
-        getText.textContent = 'Получено!'
-    })
-    .catch(() => {
-        getText.textContent = 'Не получено :('
-    })
-})
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    
-    const data = new FormData(form)
-
-    sendData({
-        url: 'https://jsonplaceholder.typicode.com/posts',
-        data: data,
-        method: 'POST'
-    })
-    .then(() => {
-        sendText.textContent = 'Отправлено!'
-    })  
-    .catch(() => {
-        getText.textContent = 'Не отправлено :('
-    })
+    getData()
 })
